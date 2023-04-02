b59215f9ffbb5b66396f8790c9a93e205b231e70
Vlada Shudegova <vlada.shudegova@gmail.com>
2023-04-03 02:40:53 +0700

    Write class Node and header for cass Node

--- Node.cpp
+++ Node.cpp
@@ -1,1 +1,43 @@
 #include "Node.h"
+
+Node::Node(int key, Node *leftChild, Node *rightChild)
+{
+    m_key = key;
+    m_leftChild = leftChild;
+    m_rightChild = rightChild;
+
+}
+
+int Node::key() const
+{
+    return m_key;
+}
+
+int Node::childrenCount() const
+{
+    return m_childrenCount;
+}
+
+void Node::setKey(const int key) {
+    m_key = key;
+}
+
+void Node::setLeftChild(Node *leftChild) {
+    m_leftChild = leftChild;
+}
+
+void Node::setRightChild(Node *rightChild) {
+    m_rightChild = rightChild;
+}
+
+int Node::getKey() const{
+    return m_key;
+}
+
+Node* Node::getLeftChild() {
+    return m_leftChild;
+}
+
+Node* Node::getRightChild() {
+    return m_rightChild;
+}
--- Node.h
+++ Node.h
@@ -1,19 +1,38 @@
 #ifndef LABS_TREE_NODE_H
 #define LABS_TREE_NODE_H
 
+/*
+ * Необходимые методы класса Node:
+ + конструкторы (по умолчанию и с параметрами);
+ + получение ключа узла (целое число);
+ + получение количества прямых потомков узла;
+ */
+
 
 class Node
 {
+private:
+    int m_key = 0;
+    int m_childrenCount = 0;
+    Node *m_leftChild = nullptr;
+    Node *m_rightChild = nullptr;
+
 public:
+    Node() = default;
     Node(int key = 0, Node *leftChild = nullptr, Node *rightChild = nullptr);
     ~Node() = default; //Удалением потомков занимается НЕ узел
     int key() const;
+    int childrenCount() const;
     void setKey(const int key);
-    //ToDo: геттеры и сеттеры потоков (по аналогии с ключом)
-private:
-    int m_key = 0;
-    Node *m_leftChild = nullptr;
-    Node *m_rightChild = nullptr;
+    void setLeftChild(Node* leftChild);
+    void setRightChild(Node* rightChild);
+    int getKey() const;
+    Node* getLeftChild();
+    Node* getRightChild();
+
+
+
+
 };
 
 
